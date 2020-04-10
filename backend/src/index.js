82cdc4751a01481aa135b59c4d43c37d6b6212d5
Andre Alvim <andre@alvim.net>
2020-04-09 23:08:28 -0300

    Configurando Rotas

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,35 @@
 const express = require("express");
+
+const app = express();
+
+app.get("/projects", (req, res) => {
+  return res.json(["Projeto 1", "Projeto 2", "projeto 3", "Projeto 4"]);
+});
+
+app.post("/projects", (req, res) => {
+  return res.json([
+    "Projeto 1",
+    "Projeto 2",
+    "projeto 3",
+    "Projeto 4",
+    "Projeto 5",
+  ]);
+});
+
+app.put("/projects/:id", (req, res) => {
+  return res.json([
+    "Projeto ALTERADO",
+    "Projeto 2",
+    "projeto 3",
+    "Projeto 4",
+    "Projeto 5",
+  ]);
+});
+
+app.delete("/projects/:id", (req, res) => {
+  return res.json(["Projeto 1", "Projeto 2", "projeto 3", "Projeto 4"]);
+});
+
+app.listen(3333, () => {
+  console.log("Back-end started!");
+});
